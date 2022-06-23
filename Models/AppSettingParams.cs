442c45817f9c906bef30bfcdb0e52aeddfa9120f
Paelyt <76490396+Paelyt@users.noreply.github.com>
2022-06-23 13:45:36 +0100

    Add files via upload

---
+++ Models/AppSettingParams.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeviceFinanceApp.Models
+{
+    public class AppSettingParams
+    {
+        public string INT_PREQUALIFY_INFO_URL { get; set; }
+        public string INT_API_KEY { get; set; }
+        public string INT_API_SECRET { get; set; }
+        public string SMS_SENDER { get; set; }
+        public string SMS_USERNAME { get; set; }
+        public string SMS_PASSWORD { get; set; }
+    }
+}
